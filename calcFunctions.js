1ef11d7a1a291925f51bfa0ce9df4fa5715b7b10
Andrew Gomez <ootbyc@gmail.com>
2019-01-15 13:24:18 -0500

    plus

--- calcFunctions.js
+++ calcFunctions.js
@@ -1,7 +1,41 @@
+let operator = ""
+let num1 = ""
+let answer = ""
+
+
 function number(num) {
+  if(operator == "+" || operator == "-" || operator == "/" || operator == "*") {
+      document.form.textview.value = "";
+      num1 += operator
+      operator = "";
+      console.log(num1)
+    }
   document.form.textview.value = document.form.textview.value + num
 }
 
 function blank() {
   document.form.textview.value = ""
+}
+
+
+function addition() {
+  if (document.form.textview.value == "/" || document.form.textview.value == "*" || document.form.textview.value == "-"){
+    document.form.textview.value = ""
+    document.form.textview.value = "+"
+  }
+ if (document.form.textview.value == ""){
+    document.form.textview.value = ""
+  } else {
+    operator = "+"
+    num1 = document.form.textview.value
+    document.form.textview.value = operator
+    console.log(num1)
+  }
+}
+
+function equals() {
+    num1 += document.form.textview.value
+    answer = eval(num1);
+    document.form.textview.value = answer
+    console.log(answer)
 }

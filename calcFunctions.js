b4b1c04a724926a449b8ab8922c4bc72e0fc7210
Andrew Gomez <ootbyc@gmail.com>
2019-01-17 13:24:10 -0500

    HELP

--- calcFunctions.js
+++ calcFunctions.js
@@ -1,41 +1,122 @@
 let operator = ""
 let num1 = ""
 let answer = ""
+let done = false
+let perce = false
 
 
 function number(num) {
-  if(operator == "+" || operator == "-" || operator == "/" || operator == "*") {
-      document.form.textview.value = "";
-      num1 += operator
+  if (document.form.textview.value == "0"){
+    document.form.textview.value = ""
+  }
+  if (document.form.textview.value == answer){
+    document.form.textview.value = ""
+  }
+
+if(operator == "+" || operator == "-" || operator == "/" || operator == "*") {
+      document.form.textview.value = num1 + operator
       operator = "";
       console.log(num1)
     }
+  if(done == true){
+    done = false
+    answer = ""
+  }
   document.form.textview.value = document.form.textview.value + num
 }
 
+
+
 function blank() {
   document.form.textview.value = ""
+  document.form.textview.value = "0"
+  num1 = ""
+  operator = ""
+}
+
+function equals() {
+    if (document.form.textview.value == num1 + operator) {
+      document.form.textview.value == num1 + operator
+    }else {
+    num1 = document.form.textview.value
+    answer = eval(num1);
+    if ( perce === true){
+      number(answer)
+      answer / 100
+    }
+    document.form.textview.value = answer
+    console.log(answer)
+    done = true
+  }
+}
+
+
+function percent() {
+  perce = true
 }
 
 
 function addition() {
-  if (document.form.textview.value == "/" || document.form.textview.value == "*" || document.form.textview.value == "-"){
-    document.form.textview.value = ""
-    document.form.textview.value = "+"
+  if(document.form.textview.value == num1 + operator){
+   document.form.textview.value = num1
   }
- if (document.form.textview.value == ""){
-    document.form.textview.value = ""
-  } else {
+
+  num1 = document.form.textview.value
+
+  if( operator == "+" || operator == "-" || operator == "/" || operator == "*") {
     operator = "+"
-    num1 = document.form.textview.value
-    document.form.textview.value = operator
-    console.log(num1)
+    document.form.textview.value = num1 + operator
+  }else{
+    document.form.textview.value = document.form.textview.value + operator
+  operator = "+"
   }
 }
 
-function equals() {
-    num1 += document.form.textview.value
-    answer = eval(num1);
-    document.form.textview.value = answer
-    console.log(answer)
+function subtraction() {
+  if(document.form.textview.value == num1 + operator){
+   document.form.textview.value = num1
+  }
+
+  num1 = document.form.textview.value
+
+  if( operator == "+" || operator == "-" || operator == "/" || operator == "*") {
+    operator = "-"
+    document.form.textview.value = num1 + operator
+  }else{
+    document.form.textview.value = document.form.textview.value + operator
+  operator = "-"
+  }
+}
+
+function multiplication() {
+  if(document.form.textview.value == num1 + operator){
+   document.form.textview.value = num1
+  }
+
+  num1 = document.form.textview.value
+
+  if( operator == "+" || operator == "-" || operator == "/" || operator == "*") {
+    operator = "*"
+    document.form.textview.value = num1 + operator
+  }else{
+    document.form.textview.value = document.form.textview.value + operator
+  operator = "*"
+  }
+
+}
+
+function division() {
+  if(document.form.textview.value == num1 + operator){
+   document.form.textview.value = num1
+  }
+
+  num1 = document.form.textview.value
+
+  if( operator == "+" || operator == "-" || operator == "/" || operator == "*") {
+    operator = "/"
+    document.form.textview.value = num1 + operator
+  }else{
+    document.form.textview.value = document.form.textview.value + operator
+  operator = "/"
+  }
 }

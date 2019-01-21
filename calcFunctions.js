a94f70a137e7d1f83e4a081f04cdf8801f925fe3
Andrew Gomez <ootbyc@gmail.com>
2019-01-21 16:14:05 -0500

    done

--- calcFunctions.js
+++ calcFunctions.js
@@ -1,122 +1,163 @@
-let operator = ""
-let num1 = ""
-let answer = ""
-let done = false
-let perce = false
-
+let answer = "";
+let good = "";
+let standby = "";
+let temp = "";
+let count = 0
 
 function number(num) {
-  if (document.form.textview.value == "0"){
-    document.form.textview.value = ""
+  if(document.getElementById("output").innerHTML == "+" || document.getElementById("output").innerHTML == "-" || document.getElementById("output").innerHTML == "*" || document.getElementById("output").innerHTML == "/"){
+    good += document.getElementById("output").innerHTML
+    document.getElementById("output").innerHTML = ""
+}  if (document.getElementById("output").innerHTML == "0"){
+    document.getElementById("output").innerHTML = ""
   }
-  if (document.form.textview.value == answer){
-    document.form.textview.value = ""
+  if (document.getElementById("output").innerHTML == answer){
+    document.getElementById("output").innerHTML = ""
   }
-
-if(operator == "+" || operator == "-" || operator == "/" || operator == "*") {
-      document.form.textview.value = num1 + operator
-      operator = "";
-      console.log(num1)
+  if (standby.length > 8){
+  }else {
+      standby = standby + num
+      temp = document.getElementById("output").innerHTML + num
+      document.getElementById("output").innerHTML = temp.toLocaleString();
+      if (document.getElementById("output").innerHTML == "-" + standby){
+      }else {
+        standby = document.getElementById("output").innerHTML
     }
-  if(done == true){
-    done = false
-    answer = ""
   }
-  document.form.textview.value = document.form.textview.value + num
 }
-
-
 
 function blank() {
-  document.form.textview.value = ""
-  document.form.textview.value = "0"
-  num1 = ""
-  operator = ""
+  document.getElementById("output").innerHTML = "0"
+  answer = ""
+  good = ""
+  standby = ""
 }
 
-function equals() {
-    if (document.form.textview.value == num1 + operator) {
-      document.form.textview.value == num1 + operator
-    }else {
-    num1 = document.form.textview.value
-    answer = eval(num1);
-    if ( perce === true){
-      number(answer)
-      answer / 100
+function equals(){
+  if (document.getElementById("output").innerHTML == "+" || document.getElementById("output").innerHTML == "-" || document.getElementById("output").innerHTML == "*" || document.getElementById("output").innerHTML == "/"){
+  }else{
+  good += standby
+  answer = eval(good)
+  document.getElementById("output").innerHTML = answer.toLocaleString();
+  if (answer == "Infinity"){
+    document.getElementById("output").innerHTML = "Error INFINITY"
+    document.body.style.backgroundImage = "url('https://i.gifer.com/iCg.gif')";
+  } else if (answer >= 1000000000) {
+    let exponent = answer.toString().length-1;
+    document.getElementById('output').innerHTML = answer / (10 ** (exponent)) + 'e' + exponent;;
     }
-    document.form.textview.value = answer
-    console.log(answer)
-    done = true
+}
+}
+
+function negate() {
+  if (document.getElementById("output").innerHTML == "-" + standby){
+    document.getElementById("output").innerHTML = standby
+  }else if (document.getElementById("output").innerHTML.indexOf("-") == 0 && document.getElementById("output").innerHTML != "-" + answer) {
+    document.getElementById("output").innerHTML = standby
+  } else if (document.getElementById("output").innerHTML == answer){
+    document.getElementById("output").innerHTML = "-" + answer
+  } else if (document.getElementById("output").innerHTML == "-" + answer){
+    document.getElementById("output").innerHTML = answer
+  }else{
+    document.getElementById("output").innerHTML = "-" + standby
+  }
+console.log()
+}
+
+
+function perce() {
+  if (document.getElementById("output").innerHTML.indexOf("-") == 0) {
+    standby = eval(standby * 0.01).toFixed(7)
+    document.getElementById("output").innerHTML = "-" + standby
+  } else {
+    standby = eval(standby * 0.01).toFixed(7)
+    document.getElementById("output").innerHTML = standby
+}
+}
+
+function pi(){
+  if(document.getElementById("output").innerHTML.indexOf(".") != -1){
+  }else {
+  if(document.getElementById("output").innerHTML == "0"){
+    document.getElementById("output").innerHTML = "3.14"
+  }else if (standby.length > 9){
+  }else{
+      document.getElementById("output").innerHTML += "3.14"
+  }
+  standby = document.getElementById("output").innerHTML
   }
 }
 
-
-function percent() {
-  perce = true
+function decimal() {
+  if (document.getElementById("output").innerHTML.indexOf(".") !== -1){
+    document.getElementById("output").innerHTML = document.getElementById("output").innerHTML
+  }else{
+    document.getElementById("output").innerHTML += "."
+  }
 }
-
 
 function addition() {
-  if(document.form.textview.value == num1 + operator){
-   document.form.textview.value = num1
-  }
-
-  num1 = document.form.textview.value
-
-  if( operator == "+" || operator == "-" || operator == "/" || operator == "*") {
-    operator = "+"
-    document.form.textview.value = num1 + operator
-  }else{
-    document.form.textview.value = document.form.textview.value + operator
-  operator = "+"
-  }
+  if(document.getElementById("output").innerHTML == "+" || document.getElementById("output").innerHTML == "-" || document.getElementById("output").innerHTML == "*" || document.getElementById("output").innerHTML == "/"){
+  document.getElementById("output").innerHTML = "+"
+} else if (standby = ""){
+} else if (document.getElementById("output").innerHTML == answer){
+  good = answer
+  document.getElementById("output").innerHTML = "+"
+}else {
+    good += document.getElementById("output").innerHTML
+  standby = ""
+  document.getElementById("output").innerHTML = "+"
+}
 }
 
 function subtraction() {
-  if(document.form.textview.value == num1 + operator){
-   document.form.textview.value = num1
-  }
-
-  num1 = document.form.textview.value
-
-  if( operator == "+" || operator == "-" || operator == "/" || operator == "*") {
-    operator = "-"
-    document.form.textview.value = num1 + operator
-  }else{
-    document.form.textview.value = document.form.textview.value + operator
-  operator = "-"
-  }
+  if(document.getElementById("output").innerHTML == "+" || document.getElementById("output").innerHTML == "-" || document.getElementById("output").innerHTML == "*" || document.getElementById("output").innerHTML == "/"){
+  document.getElementById("output").innerHTML = "-"
+} else if (standby = ""){
+} else if (document.getElementById("output").innerHTML == answer){
+  good = answer
+  document.getElementById("output").innerHTML = "-"
+}else {
+  good += document.getElementById("output").innerHTML
+  standby = ""
+  document.getElementById("output").innerHTML = "-"
+}
 }
 
 function multiplication() {
-  if(document.form.textview.value == num1 + operator){
-   document.form.textview.value = num1
-  }
-
-  num1 = document.form.textview.value
-
-  if( operator == "+" || operator == "-" || operator == "/" || operator == "*") {
-    operator = "*"
-    document.form.textview.value = num1 + operator
-  }else{
-    document.form.textview.value = document.form.textview.value + operator
-  operator = "*"
-  }
-
+  if(document.getElementById("output").innerHTML == "+" || document.getElementById("output").innerHTML == "-" || document.getElementById("output").innerHTML == "*" || document.getElementById("output").innerHTML == "/"){
+  document.getElementById("output").innerHTML = "*"
+} else if (standby = ""){
+} else if (document.getElementById("output").innerHTML == answer){
+  good = answer
+  document.getElementById("output").innerHTML = "*"
+} else {
+  good += document.getElementById("output").innerHTML
+  standby = ""
+  document.getElementById("output").innerHTML = "*"
+}
 }
 
 function division() {
-  if(document.form.textview.value == num1 + operator){
-   document.form.textview.value = num1
-  }
+  if(document.getElementById("output").innerHTML == "+" || document.getElementById("output").innerHTML == "-" || document.getElementById("output").innerHTML == "*" || document.getElementById("output").innerHTML == "/"){
+  document.getElementById("output").innerHTML = "/"
+} else if (standby = ""){
+} else if (document.getElementById("output").innerHTML == answer){
+  good = answer
+  document.getElementById("output").innerHTML = "/"
+}
+else {
+  good += document.getElementById("output").innerHTML
+  standby = ""
+  document.getElementById("output").innerHTML = "/"
+}
+}
 
-  num1 = document.form.textview.value
-
-  if( operator == "+" || operator == "-" || operator == "/" || operator == "*") {
-    operator = "/"
-    document.form.textview.value = num1 + operator
-  }else{
-    document.form.textview.value = document.form.textview.value + operator
-  operator = "/"
-  }
+function power1() {
+  standby = eval(standby * standby)
+if (standby.length > 9){
+  let exponent = answer.toString().length-1;
+  document.getElementById('output').innerHTML = answer / (10 ** (exponent)) + 'e' + exponent;;
+}
+  document.getElementById("output").innerHTML = standby
 }
